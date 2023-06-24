fd8ef697339c8433ff2b50d1f7037ee07ae9d90c
Feyorsh <ghubs25@gmail.com>
2023-06-24 10:26:48 -0400

    I still suck at nix btw

--- hosts/home.nix
+++ hosts/home.nix
@@ -1,85 +1,111 @@
 { config, lib, pkgs, user, ... }: {
-	fonts.fontconfig.enable = true;
-	home = {
-		username = "ghuebner";
-		homeDirectory = "/home/ghuebner";
+  fonts.fontconfig.enable = true;
+  home = {
+    username = "ghuebner";
+    homeDirectory = "/home/ghuebner";
+
+    packages = with pkgs; [
+      neovim
+      brightnessctl
+      wezterm
+      kitty
+      firefox-wayland # deprecated
+
+      #emacs29-pgtk
+      ((emacsPackagesFor emacs29-pgtk).emacsWithPackages (epkgs: [ epkgs.vterm epkgs.pdf-tools ]))
+      aspell
+      aspellDicts.en
+
+      brave
+      spotify
+      playerctl
+      slurp
+      grim
+
+      # fonts
+      (nerdfonts.override { fonts = [ "CascadiaCode" ]; })
+      source-sans-pro
+      sarasa-gothic
+      jetbrains-mono
+      twitter-color-emoji
+      emacs-all-the-icons-fonts
+
+      pinentry
+
+      zig
+      python312
+      nodePackages.pyright
+
+      openshift
+    ];
 
 
+    pointerCursor = {
+      gtk.enable = true;
+      name = "macOS-Monterey";
+      package = pkgs.apple-cursor;
+      size = 16;
+    };
+    stateVersion = "23.05";
 
-		packages = with pkgs; [
-			neovim
-			brightnessctl
-			wezterm
-			kitty
-			firefox-wayland # deprecated
+    # I'm not symlinking this, because init.el should exist independently of HM and Nix
+    #file.".config/emacs".source = config.lib.file.mkOutOfStoreSymlink ../config/emacs;
+  };
+  programs.home-manager.enable = true;
 
-			#emacs29-pgtk
-			((emacsPackagesFor emacs29-pgtk).emacsWithPackages (epkgs: [ epkgs.vterm epkgs.pdf-tools ]))
-			aspell
-			aspellDicts.en
+  services.playerctld.enable = true;
+  #programs.emacs = {
+  # enable = true;
+  # extraPackages = epkgs: [ epkgs.emacs-libvterm epkgs.pdf-tools ];
+  #};
 
-			brave
-			spotify
-			slurp
-			grim
+  programs.fish = {
+    enable = true;
+    interactiveShellInit = ''
+      direnv hook fish | source
 
-			# fonts
-			(nerdfonts.override { fonts = [ "CascadiaCode" ]; })
-			source-sans-pro
-			sarasa-gothic
-			jetbrains-mono
-			twitter-color-emoji
-			emacs-all-the-icons-fonts
+      if begin; [ -n "$INSIDE_EMACS" ] ; end
+         fish_default_key_bindings
+      else
+        fish_vi_key_bindings
+      end
+    '';
+    plugins = [
+      { name = "pure"; src = pkgs.fishPlugins.pure.src; }
+    ];
+  };
+  programs.direnv = {
+    enable = true;
+    nix-direnv.enable = true;
+  };
 
-			pinentry
-		];
-
-		pointerCursor = {
-			gtk.enable = true;
-			name = "macOS-Monterey";
-			package = pkgs.apple-cursor;
-			size = 16;
-		};
-		stateVersion = "23.05";
-
-		# I'm not symlinking this, because init.el should exist independently of HM and Nix
-		#file.".config/emacs".source = config.lib.file.mkOutOfStoreSymlink ../config/emacs;
-	};
-	programs.home-manager.enable = true;
-
-	programs.neovim.defaultEditor = true;
-	#programs.emacs = {
-	#	enable = true;
-	#	extraPackages = epkgs: [ epkgs.emacs-libvterm epkgs.pdf-tools ];
-	#};
-
-	programs.gpg = {
-		enable = true;
-		mutableKeys = true;
-		mutableTrust = true;
-	};
-	services.gpg-agent = {
-		enable = true;
-		pinentryFlavor = "tty";
-		#extraConfig = '' pinentry-program ${pkgs.pinentry-rofi.outPath} '';
-	};
+  programs.gpg = {
+    enable = true;
+    mutableKeys = true;
+    mutableTrust = true;
+  };
+  services.gpg-agent = {
+    enable = true;
+    pinentryFlavor = "tty";
+    #extraConfig = '' pinentry-program ${pkgs.pinentry-rofi.outPath} '';
+  };
 
 
-	nixpkgs.config = {
-		allowUnfree = true;
-	};
-	gtk = {
-		enable = true;
-		theme = {
-			name = "Tokyonight-Dark-BL";
-			package = pkgs.tokyo-night-gtk;
-		};
-#		iconTheme = {
-#			name = "Papirus-Dark";
-#			package = pkgs.papirus-icon-theme;
-#		};
-		#font = {
-			#name = "FiraCode Nerd Font Mono Medium";
-		#};
-	};
+  nixpkgs.config = {
+    allowUnfree = true;
+  };
+  gtk = {
+    enable = true;
+    theme = {
+      name = "Tokyonight-Dark-BL";
+      package = pkgs.tokyo-night-gtk;
+    };
+#   iconTheme = {
+#     name = "Papirus-Dark";
+#     package = pkgs.papirus-icon-theme;
+#   };
+    #font = {
+      #name = "FiraCode Nerd Font Mono Medium";
+    #};
+  };
 }
