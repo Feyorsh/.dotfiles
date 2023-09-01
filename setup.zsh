96b0b9c0df2957e998bffefe87b19279b1dc50b8
George Huebner <huebnerg3@gmail.com>
2023-08-31 23:35:19 -0500

    all: the purge begins

--- setup.zsh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-cd `dirname $0`
-stow -v --target=.git .githooks
-git submodule update --init --recursive
-
-
-cd starship/starship; cargo build --release; ln -sf target/release/starship /usr/local/bin/starship
