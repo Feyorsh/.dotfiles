7be9d59db3a531309a856e61df30c123ccb8e574
George Huebner <george@feyor.sh>
2025-03-22 18:46:54 -0500

    misc: add global .gdbinit
    Probably need to add some pwndbg specific stuff in the future (when I
    actually get around to using it...)

--- home/config/terminal/default.nix
+++ home/config/terminal/default.nix
@@ -7,6 +7,15 @@
 
   home.file.".hushlogin".text = "";
 
+  home.file.".gdbinit".text = ''
+    set history save on
+    set history filename ~/.gdb_history
+    set history size unlimited
+    set history remove-duplicates 10
+
+    set pagination off
+  '';
+
   programs.bat = {
     enable = true;
     config = {
