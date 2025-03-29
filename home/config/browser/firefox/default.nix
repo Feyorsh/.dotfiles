65cfc6220677556c8e94b1109c979dddbb0b962d
George Huebner <george@feyor.sh>
2025-03-29 18:03:57 -0500

    firefox: bump to nightly for profiles

--- home/config/browser/firefox/default.nix
+++ home/config/browser/firefox/default.nix
@@ -1,45 +1,10 @@
-{ pkgs, lib, ... }:
-let
-  firefoxWrapped = pkgs.symlinkJoin {
-      name = "firefox";
-      paths = [ pkgs.firefox-bin ];
-      nativeBuildInputs = [
-        (pkgs.substitute {
-          src = (pkgs.makeDarwinBundle {
-            name = "Firefox (School)";
-            exec = "firefox-profile-wrapper";
-            icon = "firefox.icns";
-          });
-          substitutions = [
-            "--replace-fail"
-            ''"Firefox (School)" "firefox-profile-wrapper"''
-            ''"Firefox (School)" "firefox-profile-wrapper" "firefox" "${lib.boolToString true}"''
-          ];
-        })
-        pkgs.makeWrapper
-      ];
-      postBuild = ''
-        makeDarwinBundlePhase
-        rm "$out/Applications/Firefox (School).app/Contents/Resources/firefox.icns"
-        cp ${../../../../assets/icons/firefox.icns} "$out/Applications/Firefox (School).app/Contents/Resources/firefox.icns"
-
-        mkdir -p $out/bin
-
-        makeWrapper $out/bin/firefox-profile-wrapper $out/Applications/Firefox.app/Contents/MacOS/firefox --add-flags '-p School'
-        # {
-        #   echo "#! ${pkgs.runtimeShell} -e"
-        #   echo "open -a "$out/Applications/Firefox.app" --args -p School" '"$@"'
-        #   echo "open -a "$out/Applications/Firefox.app" --args -p School" '"$@"'
-        # } > "$out/bin/firefox-profile-wrapper"
-        # chmod +x "$out/bin/firefox-profile-wrapper"
-      '';
-  };
-in
+{ pkgs, ... }:
 {
-  home.packages = [
-    firefoxWrapped
-    pkgs.tridactyl-native
-  ];
+  programs.firefox = {
+    enable = true;
+    package = pkgs.firefox-nightly-bin;
+    nativeMessagingHosts = [ pkgs.tridactyl-native ];
+  };
 
   xdg.configFile."tridactyl/tridactylrc".source = ./tridactylrc;
 }
