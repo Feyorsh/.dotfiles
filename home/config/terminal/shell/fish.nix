8643a73ec6b90801b4bc9368d3118d6f209d8b06
George Huebner <george@feyor.sh>
2025-07-13 20:36:50 -0400

    fish: change rinse alias to sudo darwin-rebuild

--- home/config/terminal/shell/fish.nix
+++ home/config/terminal/shell/fish.nix
@@ -17,7 +17,7 @@
       fish_vi_key_bindings
     '';
     shellAliases = {
-      rinse = "cd ~/.dotfiles; darwin-rebuild switch --flake .#Opal --option sandbox false; cd -";
+      rinse = "sudo darwin-rebuild switch --flake ~/.dotfiles#Opal";
       mkown = "sudo chown -R $USER:(id -gn)";
     };
     shellAbbrs = {
