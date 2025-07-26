49bbbe4498721348f8b5a476c0011f17ab7da9f1
George Huebner <george@feyor.sh>
2025-07-25 21:41:25 -0400

    misc: configuration for pwndbg/pwntools

--- home/config/terminal/default.nix
+++ home/config/terminal/default.nix
@@ -1,3 +1,4 @@
+{ pkgs, ... }:
 {
   imports = [
     ./shell
@@ -14,7 +15,21 @@
     set history remove-duplicates 10
 
     set pagination off
+
+    if $_regex($_gdb_setting_str("prompt"), ".*pwndbg.*")
+        set show-tips off
+    done
   '';
+
+  xdg.configFile."pwn.conf".source = (pkgs.formats.ini {}).generate "pwn.conf" {
+    # context = {
+    #   timeout = 10;
+    # };
+    update = {
+      interval = "never";
+    };
+
+  };
 
   xdg.configFile."poke/pokerc.conf".text = ''
     .set endian little
