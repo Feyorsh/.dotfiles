4d42a414da0270b7d7c6b31e76b10f65ae8f9253
George Huebner <george@feyor.sh>
2025-11-08 15:03:26 -0600

    nethack: add MAIL setup tweak nethackrc/lisp config

--- home/config/games/nethack/default.nix
+++ home/config/games/nethack/default.nix
@@ -1,8 +1,48 @@
-{ pkgs, ... }:
+{ pkgs, lib, config, ... }:
 {
   home.file.".nethackrc".source = ./nethackrc;
 
-  home.packages = with pkgs; [ nethack ];
+  home.packages = with pkgs; [
+    (nethack.overrideAttrs(prev: {
+      postPatch = (prev.postPatch or "") + ''
+        sed -e 's,-DNOMAIL ,,' \
+            -e 's,/usr/bin/true,${coreutils}/bin/true,g' \
+            -i sys/unix/hints/macosx10.14
+      '';
+    }))
 
-  programs.emacs.extraPackages = epkgs: (with epkgs; [ (pkgs.callPackage ./package.nix { inherit trivialBuild; }) ]);
+    (pkgs.writeShellScriptBin "nh-open-mu4e" ''
+      ${config.home.profileDirectory}/bin/emacsclient -n --eval "(progn (require 'mu4e) (mu4e-context-switch nil \"Personal\") (mu4e-search-bookmark \"maildir:/personal/INBOX AND flag:unread\"))"
+    '')
+  ];
+
+  launchd.agents.maildir2mbox = {
+    enable = true;
+    config = {
+      Program = lib.getExe (pkgs.writeScriptBin "maildir2mbox" ''
+        #!${pkgs.python3}/bin/python3
+        import os
+        import mailbox
+        from datetime import datetime, timedelta
+        import pathlib
+
+        MAILDIR = os.path.expanduser("~${config.home.username}/Mail/personal/INBOX")
+        MBOX = "/tmp/nh.mbox"
+
+        maildir = mailbox.Maildir(MAILDIR)
+        for msg in maildir:
+            if datetime.fromtimestamp(msg.get_date()) > datetime.now() - timedelta(minutes=5):
+                pathlib.Path(MBOX).touch()
+                break
+        maildir.close()
+      '');
+      StartInterval = 5 * 60;
+      StandardOutPath = "/tmp/nh_mail.out.log";
+      StandardErrorPath = "/tmp/nh_mail.err.log";
+      RunAtLoad = true;
+    };
+  };
+
+
+  # programs.emacs.extraPackages = epkgs: (with epkgs; [ (pkgs.callPackage ./package.nix { inherit trivialBuild; }) ]);
 }
