d6e2a2aca84075936fbce38142a254521a77653c
George Huebner <george@feyor.sh>
2026-02-27 18:25:04 -0600

    firefox: move to stable

--- home/config/browser/firefox/default.nix
+++ home/config/browser/firefox/default.nix
@@ -1,23 +1,10 @@
-{ pkgs, config, ... }:
+{ pkgs, ... }:
 {
   programs.firefox = {
     enable = true;
-    package = pkgs.firefox-nightly-bin.overrideAttrs (_: {
-      override = _: pkgs.firefox-nightly-bin;
-    });
+    package = pkgs.firefox;
     nativeMessagingHosts = [ pkgs.tridactyl-native ];
   };
-
-  home.packages = [
-    (pkgs.symlinkJoin {
-      name = "firefox";
-      paths = [];
-      postBuild = ''
-        mkdir -p $out/bin
-        ln -s ${pkgs.firefox-nightly-bin}/Applications/Firefox*.app/Contents/MacOS/firefox $out/bin/
-      '';
-    })
-  ];
 
   xdg.configFile."tridactyl/tridactylrc".source = ./tridactylrc;
 }
